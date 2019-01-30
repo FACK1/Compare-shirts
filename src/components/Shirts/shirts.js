c8a2c78a30c86348d0185905b09132612a2397e4
safaaamro <safaaamro072@gmail.com>
2019-01-30 11:53:59 +0200

    destrcture shirt #13

--- src/components/Shirts/shirts.js
+++ src/components/Shirts/shirts.js
@@ -5,8 +5,8 @@ import Shirt from "../Shirt/shirt";
 
 class Shirts extends Component {
  getShirts(){
-   const renderedShirts = shirtsList.map((shirt)=>{
-    return <Shirt imageUrl={shirt.imgUrl} shirtTitle={shirt.label} shirtPrice={shirt.price} />
+   const renderedShirts = shirtsList.map(({imgUrl,label,price})=>{
+    return <Shirt imageUrl={imgUrl} shirtTitle={label} shirtPrice={price} />
    });
    return (<div className="shirts-list"> {renderedShirts} </div>);
  }
