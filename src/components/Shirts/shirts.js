98bc02b1a64d96bffed6c6c79fc3a681c94d4d4c
safaaamro <safaaamro072@gmail.com>
2019-01-30 15:31:21 +0200

    pass selectShirt function to Shirt component #15

--- src/components/Shirts/shirts.js
+++ src/components/Shirts/shirts.js
@@ -4,13 +4,17 @@ import shirtsList from  "../../shirtsList.json";
 import Shirt from "../Shirt/shirt";
 
 class Shirts extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
  getShirts(){
    const renderedShirts = shirtsList.map(({imgUrl,label,price})=>{
-    return <Shirt imageUrl={imgUrl} shirtTitle={label} shirtPrice={price} />
+    return <Shirt imageUrl={imgUrl} shirtTitle={label} shirtPrice={price} selectShirt={this.props.selectShirt} />
    });
    return (<div className="shirts-list"> {renderedShirts} </div>);
  }
-
 
     render() {
       return this.getShirts();
