d690573e7cdde86aa310694ff178788cabfd78a3
safaaamro <safaaamro072@gmail.com>
2019-01-30 16:00:03 +0200

    Modify to change selected shirts style #15

--- src/components/Shirt/shirt.js
+++ src/components/Shirt/shirt.js
@@ -5,16 +5,20 @@ class Shirt extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      style: 'shirt-card'
+    }
     this.onShirtClick = this.onShirtClick.bind(this);
   }
 
   onShirtClick(){
     this.props.selectShirt({label: this.props.shirtTitle, price: this.props.shirtPrice});
+    this.setState({ style: 'selected-shirt-card' });
   }
 
 
     render() {
-        return (<div className="shirt-card" onClick={this.onShirtClick}>
+        return (<div className={this.state.style} onClick={this.onShirtClick}>
             <img src={this.props.imageUrl} />
             <p>{this.props.shirtTitle}</p>
             <p>{this.props.shirtPrice}</p>
