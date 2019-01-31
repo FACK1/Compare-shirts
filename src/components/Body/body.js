9da01deeadfee00fe0e9004817e042585d81f6c4
mhmdtshref <mhmdtshref@gmail.com>
2019-01-31 13:14:27 +0200

    Fix bugs #23

--- src/components/Body/body.js
+++ src/components/Body/body.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Header from '../Header/header';
 import Shirts from '../Shirts/shirts';
 import Table from '../Table/table';
-import './body.css';
 
 
 class Body extends Component {
@@ -30,11 +29,11 @@ class Body extends Component {
 
   render() {
     return (
-<div id="container">
-<Header />
-<Shirts selectShirt={this.selectShirt} />
-<Table selectedShirts={this.state.selectedShirts}/>
-</div>
+<React.Fragment>
+    <Header />
+    <Shirts selectShirt={this.selectShirt} />
+    <Table selectedShirts={this.state.selectedShirts} />
+</React.Fragment>
     );
   }
 }
